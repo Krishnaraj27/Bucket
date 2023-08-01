8bec4f52eb3e93bbf224268ba9546361f06e84fb
Krishnaraj27 <gkrishnaraj2002@gmail.com>
2023-08-01 12:39:43 +0000

    updated changes from hosting bash

--- Bucket/settings.py
+++ Bucket/settings.py
@@ -25,18 +25,18 @@ SECRET_KEY = 'django-insecure-e)a9oxtrq2*cj=d%ve0_+4%bfv2@cfu4wc%i#9$o)mlpwzpo37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
-  
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 
+    'django.contrib.staticfiles',
     'app',
     'item',
 ]
